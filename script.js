384951db95f0f441bef1c52c064d6b7d06b61e4a
Carolfons <carolsantosfonseca@outlook.com>
2023-12-15 18:46:24 -0300

    responsividade da tela

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 var tabLinks = document.getElementsByClassName("tab-links");
 var tabContents = document.getElementsByClassName("tab-contents");
+var sideMenu = document.getElementById("side-menu");
 
 function openTab(tabName){
     for (tablink of tabLinks) {
@@ -14,4 +15,11 @@ function openTab(tabName){
 
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabName).classList.add("active-tab");
+}
+
+function openMenu(){
+    sideMenu.style.right = "0";
+}
+function closeMenu(){
+    sideMenu.style.right = "-150px";
 }
